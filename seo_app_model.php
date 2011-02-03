4f115a4f57ee16d18c1a9e103c7b40db3c6c2bfa
Nick Baker <nick@webtechnick.com>
2011-02-02 20:30:30 -0700

    Code cleanup

--- seo_app_model.php
+++ seo_app_model.php
@@ -4,27 +4,27 @@ class SeoAppModel extends AppModel {
 	var $actsAs = array('Containable');
 	
 	/**
-		* Overwritable IP fields for database saving
-		*/
+	* Overwritable IP fields for database saving
+	*/
 	var $fieldsToLong = array();
 	
 	/**
-		* Custom validation.
-		* Using CakePHP IP validation would be nice, but
-		* since we're storing ips as longs in our database
-		* we need a custom validation.
-		* @param field to check
-		* @return boolean
-		*/
+	* Custom validation.
+	* Using CakePHP IP validation would be nice, but
+	* since we're storing ips as longs in our database
+	* we need a custom validation.
+	* @param field to check
+	* @return boolean
+	*/
 	function isIp($check = null){
 		$ip_to_check = array_shift($check);
 		return (ip2long($ip_to_check));
 	}
 	
 	/**
-		* Save string IPs as longs
-		* @return true
-		*/
+	* Save string IPs as longs
+	* @return true
+	*/
 	function beforeSave(){
 		foreach($this->fieldsToLong as $field){
 			if(isset($this->data[$this->alias][$field]) && !is_numeric($this->data[$this->alias][$field])){
@@ -35,9 +35,9 @@ class SeoAppModel extends AppModel {
 	}
 	
 	/**
-		* Show the IPs back out.
-		* @return formatted results
-		*/
+	* Show the IPs back out.
+	* @return formatted results
+	*/
 	function afterFind($results){
 		foreach($results as $key => $val){
 			foreach($this->fieldsToLong as $field){
@@ -50,49 +50,49 @@ class SeoAppModel extends AppModel {
 	}
 	
 	/**
-    * Overwrite find so I can do some nice things with it.
-    * @param string find type
-    * - last : find last record by created date
-    * @param array of options
-    */
-  function find($type, $options = array()){
-    switch($type){
-      case 'last':
-        $options = array_merge(
-          $options,
-          array('order' => "{$this->alias}.{$this->primaryKey} DESC")
-        );
-        return parent::find('first', $options);    
-      default: 
-        return parent::find($type, $options);
-    }
-  }
-  
-  /**
-  * Set or create the model, this is useful to find the URI
-  */
-  function createOrSetUri($model = 'SeoUri', $field = 'uri'){
-    $ModelName = Inflector::camelize($model);
-    $model_underscore = Inflector::underscore($model);
-    
-    if(isset($this->data[$ModelName][$field])){
-      $this->$ModelName->contain();
-      $this->$ModelName->recursive = -1;
-      //Find the Model, and set the id.
-      if($associated_id = $this->$ModelName->field('id', array($field => $this->data[$ModelName][$field]))){
-        $this->data[$this->alias][$model_underscore . '_id'] = $associated_id;
-      }
-      else {
-      	$save = array();
-      	$save[$ModelName][$field] = $this->data[$ModelName][$field];
+	* Overwrite find so I can do some nice things with it.
+	* @param string find type
+	* - last : find last record by created date
+	* @param array of options
+	*/
+	function find($type, $options = array()){
+		switch($type){
+		case 'last':
+			$options = array_merge(
+				$options,
+				array('order' => "{$this->alias}.{$this->primaryKey} DESC")
+				);
+			return parent::find('first', $options);    
+		default: 
+			return parent::find($type, $options);
+		}
+	}
+	
+	/**
+	* Set or create the model, this is useful to find the URI
+	*/
+	function createOrSetUri($model = 'SeoUri', $field = 'uri'){
+		$ModelName = Inflector::camelize($model);
+		$model_underscore = Inflector::underscore($model);
+		
+		if(isset($this->data[$ModelName][$field])){
+			$this->$ModelName->contain();
+			$this->$ModelName->recursive = -1;
+			//Find the Model, and set the id.
+			if($associated_id = $this->$ModelName->field('id', array($field => $this->data[$ModelName][$field]))){
+				$this->data[$this->alias][$model_underscore . '_id'] = $associated_id;
+			}
+			else {
+				$save = array();
+				$save[$ModelName][$field] = $this->data[$ModelName][$field];
 				$this->$ModelName->create();
 				$this->$ModelName->save($save);
 				$this->data[$this->alias][$model_underscore . '_id'] = $this->$ModelName->id;
 			}
-    }
-  }
-  
-  /**
+		}
+	}
+	
+	/**
 	* Return if the incoming URI is a regular expression
 	* @param string
 	* @return boolean if is regular expression (as two # marks)
@@ -102,15 +102,15 @@ class SeoAppModel extends AppModel {
 	}
 	
 	/**
-		* Returns the server IP
-		* @return string of incoming IP
-		*/
+	* Returns the server IP
+	* @return string of incoming IP
+	*/
 	function getIpFromServer(){
 		$check_order = array(
 			'HTTP_CLIENT_IP', //shared client
 			'HTTP_X_FORWARDED_FOR', //proxy address
 			'REMOTE_ADDR', //fail safe
-		);
+			);
 		
 		foreach($check_order as $key){
 			if(isset($_SERVER[$key]) && !empty($_SERVER[$key])){
