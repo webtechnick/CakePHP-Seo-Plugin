4dc55988978db0d03a36116b38bf9234f2aa005d
Nick Baker <nick@webtechnick.com>
2012-07-13 12:51:46 -0600

    updating 1.3 status code 200 merge from cakephp2.0 branch

--- seo_app_error.php
+++ seo_app_error.php
@@ -114,6 +114,17 @@ class SeoAppError extends ErrorHandler {
 				}
 				Configure::write('debug', 0);
 				header("Status: $code " . $this->SeoStatusCode->codes[$code], true, $code);
+				if($code == 200){
+					echo '<!doctype html> 
+					<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+						<head>
+							<title>&nbsp;</title>
+							<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+							<meta name="robots" content="noindex" />
+						</head>
+						<body></body>
+					</html>';
+				}
 				die();
 			}
 		}
