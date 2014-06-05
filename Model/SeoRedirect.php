4ff4b4b682a94806f2700974f7007a26edeb5a24
alan bount <alan@zeroasterisk.com>
2014-06-05 10:34:02 -0400

    Added required 'contain'  recursive=-1 on AppModel
    If AppModel has `recursive=-1` (which is a good practice)
    I was geting errors on the attempted (implicit) joins for SeoUri
    http://screencast.com/t/G1kZONw26pDt
    
    NOTE: there may be more, this was only me debugging getting some errors, not an audit

--- Model/SeoRedirect.php
+++ Model/SeoRedirect.php
@@ -16,18 +16,18 @@ class SeoRedirect extends SeoAppModel {
 			),
 		),
 	);
-	
+
 	var $belongsTo = array(
 		'Seo.SeoUri'
 	);
-	
+
 	/**
 	* Filter fields
 	*/
 	var $searchFields = array(
 		'SeoRedirect.redirect','SeoRedirect.callback','SeoRedirect.id','SeoUri.uri'
 	);
-	
+
 	/**
 	* Check if SEO already exists, if so, unset it and set the ID then save.
 	*/
@@ -35,7 +35,7 @@ class SeoRedirect extends SeoAppModel {
 		$this->createOrSetUri();
 		return true;
 	}
-	
+
 	/**
 	* This is a helper function for testing.
 	*/
@@ -43,21 +43,21 @@ class SeoRedirect extends SeoAppModel {
 		$this->uri_request = $request;
 		return 'ran_callback';
 	}
-	
+
 	/**
 	* Named scope to find list of uri -> redirect by order and approved/active
 	* @return list of active and approved uri -> redirects ordered by priority
 	*/
 	function findRedirectListByPriority(){
 		return $this->find('all', array(
+			'contain' => array($this->SeoUri->alias => 'uri'),
 			'fields' => array(
-				"{$this->SeoUri->alias}.uri",
 				"{$this->alias}.redirect",
 				"{$this->alias}.id",
 				"{$this->alias}.callback",
 				"{$this->alias}.is_nocache",
 			),
-			'order' => "{$this->alias}.priority ASC",
+			'order' => array("{$this->alias}.priority" => 'ASC'),
 			'conditions' => array(
 				"{$this->alias}.is_active" => true,
 				"{$this->SeoUri->alias}.is_approved" => true,
